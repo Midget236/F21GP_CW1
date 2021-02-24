0a5b81fd86f79b1c32e8d6b8a6a2756e86cd3960
Andrew Shaw <andrewjshaw98@gmail.com>
2021-02-24 20:25:54 +0000

    initial commit - scripts(player/enemy/coin) + navmeshcomponents

---
+++ Scripts/Text/score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public player currPlayer;
+    public int currScore;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject obj = GameObject.Find("player");
+        currPlayer = obj.GetComponent<player>();
+        scoreText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currScore = currPlayer.score;
+        scoreText.text = "SCORE: " + currScore.ToString();
+
+    }
+}
---
+++ Scripts/Text/win.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class win : MonoBehaviour
+{
+
+    public player currPlayer;
+    public Text winText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject obj = GameObject.Find("player");
+        currPlayer = obj.GetComponent<player>();
+        winText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currPlayer.win) {
+            winText.text = "YOU WIN";
+        }
+    }
+}
